6e6e41ca214e7ca4160b6b27feb3229f6a065d65
dwightdoreid <dwightdoreid@users.noreply.github.com>
2016-11-06 21:02:49 -0500

    Add files via upload

--- mqttHA_Point.ino
+++ mqttHA_Point.ino
@@ -1,22 +1,25 @@
 #include <dummy.h>
-/*
- 
-
-*/
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ESP8266WebServer.h>
 #include <EEPROM.h>
 
 // Update these with values suitable for your network.
 
 String pointID;
 char red[5];
-const char* ssid = "ssid";
-const char* password = "password";
-const char* mqttUser = "mqtt User";
-const char* mqttPw = "mqtt pw";
-IPAddress mqtt_server(192,168,4,800);
+const char* mqttUser = "mqttUser";
+const char* mqttPw = "mqttuser";
+const char* ssid = "test";
+const char* passphrase = "testpassword1";
+String esid;
+String epass = "";
+
+String st;
+String content;
+int statusCode;
+ESP8266WebServer server(80);
+IPAddress mqtt_server(192,168,0,100);
 
 WiFiClient espClient;
 //PubSubClient client(espClient);
@@ -25,28 +28,190 @@ PubSubClient client(mqtt_server, 1883, callback, espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
+unsigned long startTime;
+long timeHeld;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup_wifi() {
 
   delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  WiFi.begin();
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  Serial.println("Reading EEPROM ssid");
+  
+  for (int i = 32; i < 64; ++i)
+    {
+      esid += char(EEPROM.read(i));
+    }
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
+  
+  for (int i = 64; i < 128; ++i)
+    {
+      epass += char(EEPROM.read(i));
+    }
+  Serial.print("PASS: ");
+  Serial.println(epass);  
+         if (testWifi()) {
+          Serial.println("");
+          Serial.println("Connected to wifi network :/ ");
+          launchWeb(0);
+          return;
+        } 
+  //}
+  setupAP();
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool testWifi(void) {
+  WiFi.mode(WIFI_STA);
+  int c = 0;
+  if ( esid.length() > 1 ) {
+        Serial.println("Trying to connect to wifi net..");
+        WiFi.begin(esid.c_str(), epass.c_str());
+        while (WiFi.status() != WL_CONNECTED&&c<20) {
+          delay(500);
+          Serial.print(".");
+          c++;
+        }
+        if (WiFi.status() == WL_CONNECTED) {
+          Serial.println("Connected to wifi network :/ ");
+          return true; 
+        }
+        
+         
   }
-
-  randomSeed(micros());
-
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+} 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+void launchWeb(int webtype) {
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("Local IP: ");
   Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer(webtype);
+  // Start the server
+  server.begin();
+  Serial.println("Server started"); 
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+void setupAP(void) {
+  client.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+     {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
+      delay(10);
+     }
+  }
+  Serial.println(""); 
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      st += "<li>";
+      st += WiFi.SSID(i);
+      st += " (";
+      st += WiFi.RSSI(i);
+      st += ")";
+      st += (WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*";
+      st += "</li>";
+    }
+  st += "</ol>";
+  delay(100);
+  //WiFi.softAP(ssid, passphrase, 6);
+  WiFi.softAP(ssid);
+  Serial.println("softap");
+  launchWeb(1);
+  Serial.println("over");
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+void createWebServer(int webtype)
+{
+  if ( webtype == 1 ) {
+    Serial.println("");
+    Serial.println("webtype 1 selected");
+    server.on("/", []() {
+        IPAddress ip = WiFi.softAPIP();
+        String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+        content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+        content += ipStr;
+        content += "<p>";
+        content += st;
+        content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+        content += "</html>";
+        server.send(200, "text/html", content);  
+    });
+    server.on("/setting", []() {
+        String qsid = server.arg("ssid");
+        String qpass = server.arg("pass");
+        if (qsid.length() > 0 && qpass.length() > 0) {
+          Serial.println("clearing eeprom");
+          for (int i = 0; i < 128; ++i) { EEPROM.write(i, 0); }
+          Serial.println(qsid);
+          Serial.println("");
+          Serial.println(qpass);
+          Serial.println("");
+            
+          Serial.println("writing eeprom ssid:");
+          for (int i = 0; i < qsid.length(); ++i)
+            {
+              EEPROM.write(32+i, qsid[i]);
+              Serial.print("Wrote: ");
+              Serial.println(qsid[i]); 
+            }
+          Serial.println("writing eeprom pass:"); 
+          for (int i = 0; i < qpass.length(); ++i)
+            {
+              EEPROM.write(64+i, qpass[i]);
+              Serial.print("Wrote: ");
+              Serial.println(qpass[i]); 
+            }    
+          EEPROM.commit();
+          content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+          statusCode = 200;
+        } else {
+          content = "{\"Error\":\"404 not found\"}";
+          statusCode = 404;
+          Serial.println("Sending 404");
+        }
+        server.send(statusCode, "application/json", content);
+    });
+  } else if (webtype == 0) {
+    server.on("/", []() {
+      IPAddress ip = WiFi.localIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      server.send(200, "application/json", "{\"IP\":\"" + ipStr + "\"}");
+    });
+    server.on("/cleareeprom", []() {
+      content = "<!DOCTYPE HTML>\r\n<html>";
+      content += "<p>Clearing the EEPROM</p></html>";
+      server.send(200, "text/html", content);
+      Serial.println("clearing eeprom");
+      for (int i = 0; i < 128; ++i) { EEPROM.write(i, 0); }
+      EEPROM.commit();
+    });
+  }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 void callback(char* topic, byte* payload, unsigned int lent) {
@@ -124,6 +289,7 @@ void redEEPromRead(){
 
 void reconnect() {
   // Loop until we're reconnected
+  Serial.println("Reconnecting MQTT...");
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
@@ -132,11 +298,20 @@ void reconnect() {
     if (client.connect(clientId.c_str(),mqttUser, mqttPw)) {
       Serial.println("connected");
       char pnt[4];
-      pointID.toCharArray(pnt,pointID.length()+1);
-      client.subscribe(pnt);
+      if (pointID.length()==0){
+        pointID = "Test";
+      }
+      else{
+        pointID.toCharArray(pnt,pointID.length()+1);
+        client.subscribe(pnt);
+      }
+      
       Serial.print("subscribed to: ");
       Serial.println(pnt);
       client.loop();
+      Serial.print("MQTT Connection status:  ");
+      Serial.println(client.connected());
+      delay(500);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -166,7 +341,11 @@ void setup() {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
 
-  if (!client.connect("arduinoClient", "mqttUser", "mqttuse")) {
+//  if (!client.connect("arduinoClient", "mqttUser", "mqttuse")) {
+//    reconnect();
+//  }
+  if (!client.connected() && (WiFi.status() == WL_CONNECTED)) {
+    Serial.println("rwwwiaiau");
     reconnect();
   }
   /*else
@@ -177,10 +356,23 @@ void loop() {
   client.subscribe("inTopic");
   client.loop();
   
-   if (digitalRead(0)==false)
-  {
-    Serial.println("button pressed");
-    joinControlNet();
-    delay(250);
+//   if (digitalRead(0)==false)
+//  {
+//    Serial.println("button pressed");
+//    joinControlNet();
+//    delay(250);
+//  }
+
+   if (digitalRead(0)==false){
+    startTime=millis();  
+    while (digitalRead(0)==false){
+        timeHeld=millis()-startTime;
+        Serial.print("Timeheld is: ");
+        Serial.println(timeHeld);        
+     }    
   }
+  if (timeHeld>1000) {Serial.println("time is long time");setupAP();}
+  if (timeHeld>50 && timeHeld<750) {Serial.println("time is good");joinControlNet();}
+  timeHeld=0;
+   
 }
