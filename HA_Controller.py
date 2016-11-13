d7a5fc3afd9e08eb9fecc22c4cb168338cb5677d
dwightdoreid <dwightdoreid@users.noreply.github.com>
2016-11-13 15:31:48 -0500

    Add files via upload

---
+++ HA_Controller.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 13 09:54:29 2016
+
+@author: dwight
+"""
+
+import paho.mqtt.client as mqtt
+import time
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    #client.subscribe("$SYS/#")
+    client.subscribe("L007")
+    client.subscribe("L005")
+    client.subscribe("newDevice/#")
+	client.subscribe("remDevice")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    if msg.topic=="newDevice":
+        print("new device detected")
+        time.sleep(1)
+        if str(msg.payload) == "lamp/":
+            client.publish("newDeviceAdd",payload="L005",qos=0,retain=False)
+
+client = mqtt.Client(client_id="ha_cntrlr")
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set("mqttUser", password="mqttuser")
+client.connect("192.168.0.106", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
